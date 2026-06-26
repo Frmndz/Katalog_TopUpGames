13a5e871bd634cab0b278dfe15d1c2279545bed5
Frmndz <firmandzaki6@gmail.com>
2026-06-26 14:53:54 +0700

    Menambahkan route itemscontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return redirect()->route('items.index');
+});
+Route::resource('items', ItemsController::class);
